54a6003c24d1643c24020b3bc192004f3ae15080
DJ Daniels <djdanielsh@gmail.com>
2026-03-14 20:04:42 -0700

    Update p-arr to v0.3.1

--- Casks/p-arr.rb
+++ Casks/p-arr.rb
@@ -1,6 +1,6 @@
 cask "p-arr" do
-  version "0.3.0"
-  sha256 "015d0ab4842ea8b2146ab0642b98e1cd5296830808a7b163723ce3b69ec8fedb"
+  version "0.3.1"
+  sha256 "7b0e09c4efaf650ca31dc8d369c6b16bd076df6578807e90d5d99c2eebcb39bb"
 
   url "https://github.com/TheBranchDriftCatalyst/pr-widget/releases/download/v#{version}/P-Arr-#{version}.zip"
   name "P-Arr"
