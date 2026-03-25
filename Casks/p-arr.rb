2e8aca741b7d36431825013cac5e2532e5113169
DJ Daniels <djdanielsh@gmail.com>
2026-03-25 14:33:53 -0700

    Update p-arr to v0.7.2

--- Casks/p-arr.rb
+++ Casks/p-arr.rb
@@ -1,6 +1,6 @@
 cask "p-arr" do
-  version "0.7.1"
-  sha256 "e03d4f3827c32c677222d68bb9375379e9f8f63d8a0703ce52959cf371fb85d1"
+  version "0.7.2"
+  sha256 "69fe71b6a16abca12a2079959c247fa7373eec332bc15887ed1d943bf9f0ed01"
 
   url "https://github.com/TheBranchDriftCatalyst/pr-widget/releases/download/v#{version}/P-Arr-#{version}.zip"
   name "P-Arr"
